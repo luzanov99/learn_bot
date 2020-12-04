8fb0bc272d71406ca75cc1a74c89f99541f8ceac
Luazanov Yevgen <luzanov.zena@gmail.com>
2020-12-04 14:07:34 +0200

    add game, calculate

--- 1.py
+++ 1.py
@@ -1,4 +1,15 @@
-import ephem 
-active_planet=ephem.Uranus('2020/10/10')
-constellation = ephem.constellation(active_planet)
-print(constellation)
+import copy
+spisok=['москва', 'киев','астрахань','новосибирск','алжир','донецк','винница']
+states={}
+def game(chat_id,stroka):
+    
+    print(chat_id)
+    if chat_id not in states:
+        states[chat_id] = copy.copy(spisok)
+    states[chat_id].remove(stroka)
+
+game(1,'москва')
+
+game(2,'киев')   
+
+print(states)     
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import settings
 import ephem 
+import copy
 import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -10,10 +11,81 @@ PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 
+#Шаблон списка городов для пользователя
+spisok=['москва', 'киев','ровм','новосибирск','алжир','ара','винница']
+#Словарь городов для пользователей
+states={}
+#Словарь количества ходов игры для каждого пользователя
+games={}
+#Словарь последних букв для пользователей
+lst={}
+def game(update, context):
+    chat_id=update.message.chat.id
+    #Проверка на существование уже активной игры польщзователя
+    #Если нет то инициализация начальных параметров
+    if chat_id not in lst:
+        lst[chat_id]=''
+    if chat_id not in states:
+        states[chat_id] = copy.copy(spisok)
+
+    if chat_id not in games:
+        games[chat_id]=0
+    #Если первый ход
+    if games[chat_id]==0:
+        games[chat_id]+=1
+        stroka=update.message.text.split(' ',1)
+        #Проверка на наличие города  в списке 
+        if stroka[1] in states[chat_id] : 
+            #Количество выводимых ответов ботом должно быть 1
+            kol_otvet=0
+            #Удаляем использованное  пользователем слово
+            states[chat_id].remove(stroka[1])
+            #Ищем в списке подходящий город
+            for element in states[chat_id]:
+                if stroka[1][-1]==element[0]:
+                    kol_otvet+=1
+                    #Вывод ответа, фиксирование последний буквы бота, удаление города из списка
+                    if kol_otvet==1:
+                        update.message.reply_text(element)
+                        lst[chat_id]=element[-1]
+                        states[chat_id].remove(element)
+                       
+                    else: return
+        #Если названия города нет в списке вывод ошибки    
+        else:
+            update.message.reply_text("Вы ввели неправильную букву или такого слова нет")
+
+    else:
+    #Если больше 1 хода
+        games[chat_id]+=1
+        stroka=update.message.text.split(' ',1)
+        #Проверка на наличие слова в списке и совпадение последней буквы слова бота
+        if stroka[1] in states[chat_id]  and stroka[1][0]==lst[chat_id]: 
+           
+            kol_otvet=0
+            states[chat_id].remove(stroka[1])
+            for element in states[chat_id]:
+                if stroka[1][-1]==element[0]:
+               
+                    kol_otvet+=1
+                    if kol_otvet==1:
+                    
+                        update.message.reply_text(element)
+                        lst[chat_id]=element[-1]
+                        states[chat_id].remove(element)
+                        print(states[chat_id])
+                    else: return
+        else:
+            update.message.reply_text("Вы ввели неправильную букву или такого слова нет")
+            
+        
+        
+
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
+#Функция показа созвездия 
 def show_planet(update, context):
     now = datetime.datetime.now()
     print('Вызван /planet')
@@ -51,7 +123,43 @@ def show_planet(update, context):
         update.message.reply_text("Такой планеты нет в базе данных")
 
     
-        
+ #Калькулятор-функция       
+def calculate(update, context):
+    stroka=update.message.text.split(' ',1)
+    result=0
+    for i in range(0,len(stroka[1])):
+        if stroka[1][i]==' ':
+            update.message.reply_text("Введите выражение без пробелов")
+            return
+    
+    for i in range(0,len(stroka[1])):
+        if stroka[1][i]=='+':
+            argument1=float(stroka[1][0:i])
+            argument2=float(stroka[1][i+1:len(stroka[1])])
+            result=argument2+argument1
+            update.message.reply_text(result)
+        if stroka[1][i]=='-':
+            argument1=float(stroka[1][0:i])
+            argument2=float(stroka[1][i+1:len(stroka[1])])
+            result=argument1-argument2
+            update.message.reply_text(result)
+        if stroka[1][i]=='/':
+            argument1=float(stroka[1][0:i])
+            argument2=float(stroka[1][i+1:len(stroka[1])])
+            if argument2 ==0.0:
+                update.message.reply_text("На 0 делить нельзя")
+                return
+            else:
+                result=argument1/argument2
+                update.message.reply_text(result)
+        if stroka[1][i]=='*':
+            argument1=float(stroka[1][0:i])
+            argument2=float(stroka[1][i+1:len(stroka[1])])
+            result=argument1*argument2
+            update.message.reply_text(result)
+    
+
+
 
 
 
@@ -68,6 +176,8 @@ def main():
     dp=mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", show_planet))
+    dp.add_handler(CommandHandler("calc", calculate))
+    dp.add_handler(CommandHandler("cities", game))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Бот стартовал")
     mybot.start_polling()
